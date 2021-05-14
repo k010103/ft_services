85501920aed26b60adbe3f903143345dc481b67d
junmkang <k010103@naver.com>
2021-05-14 16:13:15 +0900

    nginx completion

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
 #setup.sh
+
 # "minikube delete --purge --all" and "docker system prune";
 # minikube start를 입력해야만 제대로 된 포트에 연결이 되었다고 나옴.
 export MINIKUBE_HOME=/goinfre/$USER # export 적용 방법 몰름.
@@ -9,7 +9,7 @@ brew upgrade minikube kubectl
 minikube start --driver=virtualbox
 eval $(minikube -p minikube docker-env)
 
-# file 이름을 변경. addresses
+# file 내용 변경. addresses format
 MINIKUBE_IP=$(minikube ip)
 sed "s/MINIKUBE_IP/$MINIKUBE_IP/g" ./srcs/yamls/config_format.yaml > ./srcs/yamls/config.yaml
 sed "s/MINIKUBE_IP/$MINIKUBE_IP/g" ./srcs/images/nginx/srcs/nginx_format.conf > ./srcs/images/nginx/srcs/nginx.conf
@@ -52,4 +52,6 @@ kubectl apply -f ./srcs/yamls/phpmyadmin.yaml
 # kubectl delete -f ()
 # kubectl apply -f ()
 
+# minikube start를 해야지만 virtualBOX가 연동이 되는 것을 볼 수 있음.
 # 현재 minikube가 돌아가고 있는 상황이지만 minikube stop을 했을때 minikube를 찾지못하는 경우가 있다.
+# export 를 .sh에서 적용시킬려고 하면 안되는것을 볼 수 있다.
