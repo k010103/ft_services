2229ac01b1cd2e9a992cfb6e42ebfbb4d932c059
junmkang <k010103@naver.com>
2021-06-14 22:27:06 +0900

    grafana admin error

--- srcs/images/grafana/srcs/entrypoint.sh
+++ srcs/images/grafana/srcs/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
+# --config 재정의
 (grafana-server \
-    -config /etc/grafana/grafana.ini \
-    -homepath /usr/share/grafana)
+    --config /etc/grafana/grafana.ini \
+    --homepath /usr/share/grafana)
 
