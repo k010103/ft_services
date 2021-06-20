d1024452cda2acebdb2e5ef46ddb58a90d45582f
junmkang <k010103@naver.com>
2021-06-21 00:37:00 +0900

    phpmyadmin http 접속 오류

--- srcs/images/mysql/srcs/wordpress.sql
+++ srcs/images/mysql/srcs/wordpress.sql
@@ -107,8 +107,8 @@ CREATE TABLE `wp_options` (
 --
 
 INSERT INTO `wp_options` (`option_id`, `option_name`, `option_value`, `autoload`) VALUES
-(1, 'siteurl', 'http://192.168.99.252:5050', 'yes'),
-(2, 'home', 'http://192.168.99.252:5050', 'yes'),
+(1, 'siteurl', 'http://192.168.99.254:5050', 'yes'),
+(2, 'home', 'http://192.168.99.254:5050', 'yes'),
 (3, 'blogname', 'test', 'yes'),
 (4, 'blogdescription', 'Just another WordPress site', 'yes'),
 (5, 'users_can_register', '0', 'yes'),
--- srcs/reset/nginre.sh
+++ srcs/reset/nginre.sh
@@ -2,13 +2,13 @@ kubectl delete -f ../yamls/nginx.yaml
 eval $(minikube docker-env)
 sleep 8
 docker ps #> /dev/null 2>&1
-docker rmi nginx #> /dev/null 2>&1
+docker rmi nginx_service #> /dev/null 2>&1
 docker images #> /dev/null 2>&1
-docker rmi nginx #> /dev/null 2>&1
+docker rmi nginx_service #> /dev/null 2>&1
 docker ps #> /dev/null 2>&1
-docker rmi nginx #> /dev/null 2>&1
+docker rmi nginx_service #> /dev/null 2>&1
 docker images #> /dev/null 2>&1
-docker rmi nginx #> /dev/null 2>&1
+docker rmi nginx_service #> /dev/null 2>&1
 echo "[build nginx...!!!]"
-docker build -t nginx ../images/nginx #> /dev/null 2>&1
+docker build -t nginx_service ../images/nginx #> /dev/null 2>&1
 kubectl apply -f ../yamls/nginx.yaml
